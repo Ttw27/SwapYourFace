740bc1ece4fbd77b56d10dee249831357c2c3148
Ttw27 <tim@hugehoods.co.uk>
2026-03-27 14:40:05 +0000

    Add files via upload

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,38 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Toaster } from "@/components/ui/sonner";
 import { useStore } from "@/store/useStore";
 
+const API = `${process.env.REACT_APP_BACKEND_URL}/api`;
+
+// Inject Google tag
+function loadGoogleTag(tagId) {
+  if (!tagId || document.getElementById('gtag-script')) return;
+  const script = document.createElement('script');
+  script.id = 'gtag-script';
+  script.async = true;
+  script.src = `https://www.googletagmanager.com/gtag/js?id=${tagId}`;
+  document.head.appendChild(script);
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){window.dataLayer.push(arguments);}
+  window.gtag = gtag;
+  gtag('js', new Date());
+  gtag('config', tagId);
+}
+
+// Inject Facebook Pixel
+function loadFacebookPixel(pixelId) {
+  if (!pixelId || window.fbq) return;
+  !function(f,b,e,v,n,t,s){
+    if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+    n.queue=[];t=b.createElement(e);t.async=!0;
+    t.src=v;s=b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t,s)}(window, document,'script',
+    'https://connect.facebook.net/en_US/fbevents.js');
+  window.fbq('init', pixelId);
+  window.fbq('track', 'PageView');
+}
+
 // Pages
 import HomePage from "@/pages/HomePage";
 import GalleryPage from "@/pages/GalleryPage";
@@ -28,6 +60,14 @@ export default function App() {
 
   useEffect(() => {
     fetchPricing();
+    // Load tracking pixels from backend config
+    fetch(`${API}/tracking-config`)
+      .then(r => r.ok ? r.json() : {})
+      .then(config => {
+        if (config.google_tag_id) loadGoogleTag(config.google_tag_id);
+        if (config.facebook_pixel_id) loadFacebookPixel(config.facebook_pixel_id);
+      })
+      .catch(() => {});
   }, [fetchPricing]);
 
   return (
